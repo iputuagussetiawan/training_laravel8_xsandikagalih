eb7ea58da03b0c21c5ee224cce42ca27ea486a57
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-04-24 12:53:20 +0800

    routing

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="zxx">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Agus | About</title>
+        <link rel="icon" type="image/png" href="assets/images/myicon.png" />
+        <link rel="stylesheet" href="css/style.css"> 
+    </head>
+        <h1>About</h1>
+        <h3><?php echo $name?></h3>
+        <p><?php echo $email?></p>
+        <img src="img/<?php echo $image?>" alt="<?php echo $name?>">
+        {{-- <script src="assets/plugin/swiper/js/main.js"></script> --}}
+    <body>
+   </body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="zxx">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Agus | Post</title>
+        <link rel="icon" type="image/png" href="assets/images/myicon.png" />
+        <link rel="stylesheet" href="css/style.css"> 
+    </head>
+        <h1>Posts</h1>
+        {{-- <script src="assets/plugin/swiper/js/main.js"></script> --}}
+    <body>
+   </body>
+</html>
