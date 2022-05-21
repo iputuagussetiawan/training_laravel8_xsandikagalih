60274f1d9af0739292a0d03bcccd61b4a02c31a8
DESKTOP-DE46NCF\LENOVO <agussetiawaniputu@gmail.com>
2022-05-21 21:04:50 +0800

    import post view

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,7 +3,41 @@
 @extends('layouts.main')
 @section('container')
     <h1 class="mb-5">{{ $title }}</h1>
-    @foreach ($posts as $post )
+    @if ($posts->count())
+        <div class="card mb-3">
+            {{-- <img src="..." class="card-img-top" alt="..."> --}}
+            <div class="card-body">
+                <h5 class="card-title">{{ $posts[0]->title }}</h5>
+                <small>
+                    <p> By : <a href="/authors/{{ $posts[0]->author->username }}">{{ $posts[0]->author->name }}</p>
+                        <p>in : <a href="/categories/{{$posts[0]->category->slug}}">{{ $posts[0]->category->name }}</a>{{ $posts[0]->created_at->diffForHumans() }}</p>
+                        <p class="card-text">{{ $posts[0]->excerpt }}</p>
+                </small>
+             <a href="/posts/{{ $posts[0]->slug }}">Read More..</a>
+            </div>
+        </div>
+    @else
+    <p>No Post Found</p>
+    @endif
+
+    <div class="container">
+        <div class="row">
+            @foreach ($posts->skip(1) as $post )
+            <div class="col-md-4 mb-3">
+                <article class="mb-5">
+                    <h2>
+                        <a href="/posts/{{ $post->slug }}">  {{ $post->title }}</a>    
+                    </h2>
+                    <p> By : <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</p>
+                    <p>in : <a href="/categories/{{$post->category->slug}}">{{ $post->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}</p>
+                    <p>{{ $post->body }}</p>
+                </article>
+            </div>
+            @endforeach
+        </div>
+    </div>
+{{--    
+    @foreach ($posts->skip(1) as $post )
     <article class="mb-5">
         <h2>
             <a href="/posts/{{ $post->slug }}">  {{ $post->title }}</a>    
@@ -12,5 +46,5 @@
         <p>in : <a href="/categories/{{$post->category->slug}}">{{ $post->category->name }}</a></p>
         <p>{{ $post->body }}</p>
     </article>
-    @endforeach
+    @endforeach --}}
 @endsection
